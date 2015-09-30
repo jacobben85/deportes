0c5714a88d44dcf770cfab48b712c20e5ce83386
jacobben85 <jacobben85@gmail.com>
2015-09-30 11:31:01 -0400

    Feed Processor
    1. Feed processor
    2. Utils scripts

---
+++ src/main/java/com/univision/deportes/alerts/Notifications.java
@@ -0,0 +1,7 @@
+package com.univision.deportes.alerts;
+
+/**
+ * Created by jbjohn on 9/29/15.
+ */
+public class Notifications {
+}
---
+++ src/test/java/com/univision/deportes/feedsyn/FeedProcessorTest.java
@@ -0,0 +1,107 @@
+package com.univision.deportes.feedsyn;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.jayway.awaitility.Awaitility.await;
+
+/**
+ * Created by jbjohn on 9/30/15.
+ */
+public class FeedProcessorTest {
+
+    private static FeedProcessor fp;
+    private static String expectedStats;
+    private static String expectedComments;
+    private static String scheduleResults;
+    private static String eventId = "832681"; // Roma v Barcelona
+    private static String season = "2015";
+    private static String league = "5"; // Champions League
+    private static String team = "178"; // Barcelona
+
+
+    @BeforeClass
+    public static void setUp() throws IOException, URISyntaxException {
+        fp = new FeedProcessor();
+        expectedStats = fp.processFeed("stats", eventId);
+        expectedComments = fp.processFeed("commentary", eventId);
+        scheduleResults = fp.processFeed("schedule", eventId);
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[100][0]);
+    }
+
+    @Test
+    public void testProcessFeedStats() throws InterruptedException {
+        String response = null;
+        try {
+            response = fp.processFeed("stats", eventId);
+            JSONAssert.assertEquals(expectedStats, response, false);
+        } catch (AssertionError e) {
+            if (response != null) {
+                expectedStats = response;
+            }
+            throw new AssertionError(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        await().until(() ->
+        {
+            return delayMethod();
+        });
+    }
+
+    @Test
+    public void testProcessFeedComments() throws InterruptedException {
+        String response = null;
+        try {
+            response = fp.processFeed("commentary", eventId);
+            JSONAssert.assertEquals(expectedComments, response, false);
+        } catch (AssertionError e) {
+            if (response != null) {
+                expectedComments = response;
+            }
+            throw new AssertionError(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        await().until(() ->
+        {
+            return delayMethod();
+        });
+    }
+
+    @Test
+    public void testProcessFeedScheduleResults() throws InterruptedException {
+        String response = null;
+        try {
+            response = fp.processFeed("schedule", eventId);
+            JSONAssert.assertEquals(scheduleResults, response, false);
+        } catch (AssertionError e) {
+            if (response != null) {
+                scheduleResults = response;
+            }
+            throw new AssertionError(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        await().until(() ->
+        {
+            return delayMethod();
+        });
+    }
+
+    private boolean delayMethod() throws InterruptedException {
+        Thread.sleep(9000);
+        return true;
+    }
+}
