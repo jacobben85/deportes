45b239ac47013239de36e8b79b4d8967c3d8cb95
jacobben85 <jacobben85@gmail.com>
2016-01-05 14:27:25 -0500

    Adding ability to test push notifications

---
+++ src/test/java/com/univision/deportes/xmlteam/PostToLocalTest.java
@@ -0,0 +1,28 @@
+package com.univision.deportes.xmlteam;
+
+import org.junit.Test;
+
+/**
+ * Local post test
+ */
+public class PostToLocalTest {
+
+    @Test
+    public void testFetchLinksAndProcess() throws Exception {
+        String eventId = "803840";
+        String startTime = "P5D";
+
+        String fixtureKeys = "event-stats,event-stats-progressive";
+        String url = "http://sw5staging.xmlteam.com/api/feeds?" +
+                "start=" + startTime +
+                "&publisher-keys=optasports.com" +
+                "&sport-keys=15054000" +
+                "&fixture-keys=" + fixtureKeys +
+                "&format=xml" +
+                "&event-keys=EFBO" + eventId;
+        System.out.println(url);
+
+        PostToLocal postToLocal = new PostToLocal();
+        postToLocal.fetchLinksAndProcess(url);
+    }
+}
