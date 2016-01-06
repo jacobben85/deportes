fde142718f91f28636278271189632c8cc90d9e9
jacobben85 <jacobben85@gmail.com>
2016-01-06 17:19:32 -0500

    Test updates

--- src/test/java/com/univision/deportes/xmlteam/StandaloneTest.java
+++ src/test/java/com/univision/deportes/xmlteam/StandaloneTest.java
@@ -15,9 +15,7 @@ public class StandaloneTest {
 
             String folder = "processed";
             String eventId = "787816"; //EFBO787816
-            //String startTime = "20150922T010000-0400";
-            String startTime = "PT3H";
-            //String endTime = "20150924T000000-0400";
+            String startTime = "PT1H";
 
             //String fixtureKeys = "event-stats,event-stats-progressive";
             String url = "http://sw5staging.xmlteam.com/api/feeds?" +
@@ -25,31 +23,13 @@ public class StandaloneTest {
                     "&publisher-keys=optasports.com" +
                     "&sport-keys=15054000" +
                     //"&fixture-keys=" + fixtureKeys +
-                    "&format=xml";
                     //"&event-keys=EFBO" + eventId;
+                    "&format=xml";
+
             System.out.println(url);
             standalone.fetchLinksAndProcess(url, folder);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-//
-//    @Test
-//    public void testProcessOther() {
-//        Standalone standalone = new Standalone();
-//        try {
-//
-//            String eventId = "803172";
-//            String period = "start=P3M";
-//
-//            String url = "http://feed5.xmlteam.com/api/feeds?" +
-//                    "&publisher-keys=optasports.com" +
-//                    "&format=xml" +
-//                    period +
-//                    "&event-keys=EFBO";
-//            standalone.fetchLinksAndProcess(url + eventId, eventId);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
